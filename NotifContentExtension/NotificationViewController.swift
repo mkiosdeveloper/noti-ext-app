4fc523a892db615a1a3abf6a8752ce565575df4e
Mohammad Alkandari <marshal.hurdle-0d@icloud.com>
2023-07-20 23:14:00 +0300

    commit - this POC is ready to demo and take notes

---
+++ NotifContentExtension/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  NotifContentExtension
+//
+//  Created by Warba on 19/07/2023.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
